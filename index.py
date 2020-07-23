5d044b96c84fa05bb785a1a1c606b6ca3bf59d17
Yuchao Yao <www.yaoyuchao@qq.com>
2020-07-24 00:49:10 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,139 @@
+import configparser
+import os
+# 简单邮件传输协议
+import smtplib
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.utils import formataddr
+
+from utils import Utils
+
+class Text(object):
+
+
+    # 加载配置文件
+    def loading(self):
+        conf = configparser.ConfigParser()
+        conf.read(r'AccountInfo.ini', encoding="utf-8")
+        self.username = conf['MESSAGE']['account']
+        self.password = conf['MESSAGE']['password']
+        self.flag = conf['MESSAGE']['flag']
+        self.from_email = conf['MESSAGE']['from_email']
+        self.to_email = conf['MESSAGE']['to_email']
+        self.key = conf['MESSAGE']['key']
+        self. message_file= 'data/upload.txt'
+        self.successflag = '失败'
+
+    # 日志
+    def logging(self, level, text):
+        shijian = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        self.log = self.log + shijian + "  " + level + "  " + text + "\n"
+
+    def uploadmessage(self):
+
+            # cookie_file = os.path.dirname(os.path.abspath(__file__)) + os.sep + "data" + os.sep + Utils.COOKIE_FILE_NAME
+            # print("use username and password to upload message, cookie file is save to " + cookie_file)
+            LogInMsg, cookie = Utils.get_cookie_from_login(self.username, self.password)
+            if LogInMsg == "ok":
+                # cookie = Utils.load_cookie_from_file(cookie_file)
+                upload_message = Utils.load_upload_message_file(self.message_file)
+                UpLoadMsg = Utils.upload_ncov_message(cookie, upload_message=upload_message)
+                if UpLoadMsg == "ok":
+                    self.logging("INFO",upload_message)
+                    self.msg = str(upload_message)
+                    self.successflag = '成功'
+                else:
+                    self.logging("ERROR",UpLoadMsg)
+                    self.msg = str(UpLoadMsg)
+
+            else:
+                print(LogInMsg)
+                self.logging("ERROR",LogInMsg)
+                self.msg = str(LogInMsg)
+
+
+    def send_email(self):
+
+        today_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+        # 设置邮箱的域名
+        HOST = 'smtp.qq.com'
+        # 设置邮件标题
+        SUBJECT = '%s : %s 晨午晚检打卡情况 ' % (self.successflag, today_time)
+        # 设置发件人邮箱
+        FROM = self.from_email
+        # 设置收件人邮箱
+        TO = self.to_email  # 可以同时发送到多个邮箱
+        message = MIMEMultipart('related')
+
+        # 发送邮件正文到对方的邮箱中
+        # with open("log.log", "r") as logfile:  # 打开文件
+        #     data = logfile.read()  # 读取文件
+        zhengwen = "晨午晚检打卡情况：%s\n" % self.msg
+        zhengwen = zhengwen + "\n\n--------日志情况--------\n" + self.log
+        # logfile.close()
+
+
+        my_sender = self.from_email
+        my_pass = self.key  # 发件人邮箱密码(当时申请smtp给的口令)
+        my_user = self.to_email  # 可以同时发送到多个邮箱
+
+
+        try:
+            msg = MIMEText(zhengwen, 'plain', 'utf-8')
+            msg['From'] = formataddr(["晨午检脚本", my_sender])
+            # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+            msg['To'] = formataddr(["填报结果", my_user])
+            # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+            msg['Subject'] = '%s : %s 晨午晚检打卡情况 ' % (self.successflag, today_time)
+            # 邮件的主题，也可以说是标题
+
+            server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+            # 发件人邮箱中的SMTP服务器，端口是465
+            server.login(my_sender, my_pass)
+            # 括号中对应的是发件人邮箱账号、邮箱密码
+            server.sendmail(my_sender, [my_user, ], msg.as_string())
+            # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+            server.quit()  # 关闭连接
+        except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+            print("邮件发送失败:%s" % Exception)
+
+        print("邮件发送成功")
+
+
+
+    def main(self):
+        self.log = ""
+        try:
+            print("正在获取数据")
+            self.logging("INFO", "正在获取数据")
+            self.loading()
+
+            print("正在填报报表")
+            self.logging("INFO", "正在填报报表")
+            self.uploadmessage()
+
+            print("要发送的邮件内容")
+            self.logging("INFO", "要发送的邮件内容：%s" % self.msg)
+
+
+        except KeyError:
+            print("数据错误，请检查 相关信息.ini")
+            self.logging("ERROR", "数据错误，请检查 相关信息.ini")
+
+        except:
+            print("未知错误")
+            self.msg = '！！！ 未知错误 ！！！ 赶快手动填 ！！！'
+            self.logging("ERROR", "！！！ 未知错误 ！！！ 赶快手动填 ！！！")
+
+        finally:
+            if self.flag == '1':
+                self.send_email()
+                print("已经执行发送邮件任务")
+
+
+def main_handler(agrs1,agrs2):
+    Text().main()
+
+
---
+++ readme.md
@@ -0,0 +1,59 @@
+# 西安电子科技大学晨午晚检自动填报工具
+
+#### 本代码缝合了https://github.com/abadfox233/ncov和https://github.com/ZimoLoveShuang/auto-submit两位大佬的作品，仅供学习交流使用，请在下载2小时内删除
+
+#### 本项目仅供学习交流使用，如作他用所承受的任何直接、间接法律责任一概与作者无关
+
+#### 如果此项目侵犯了您或者您公司的权益，请立即联系我删除
+
+
+
+# 注意
+
+本脚本只内置了南校区的经纬度,**只适用于南校区在校的同学**
+
+
+
+# 项目说明
+
+- `data` 需要提交给网站的信息
+- `AccountInfo.ini` 登录的配置文件，用来保存帐号密码以及收发邮箱信息和邮箱key（只支持QQ邮箱）
+- `index.py` 完成自动提交的py脚本
+- `utils` 帮助生成默认项配置的py脚本
+- `requirements.txt` py依赖库以及版本说明文件
+- `层文件.zip` 打包好的云函数依赖库文件
+
+
+
+# 使用方式
+
+### 配合腾讯云函数使用（免费）
+
+1. clone 或者 下载 此仓库到本地
+
+   ```
+   git clone https://github.com/HPShark/xdu_chenwuwanjian.git
+   ```
+
+2. 打开本地仓库文件夹，配置`AccountInfo.ini`中对应的信息，**注意这里的学号和密码都是教务系统的学号和密码，key是邮箱的密码**
+
+3. 打开百度搜索[腾讯云函数](https://console.cloud.tencent.com/scf/index?rid=1)，注册认证后，进入控制台，点击左边的层，然后点新建，名称随意，然后点击上传zip，选择项目中的`层文件.zip`上传，然后选择运行环境`python3.6`，然后点击确定，耐心等待一下，上传依赖包需要花费的时间比较长 [![新建腾讯云函数依赖](https://github.com/ZimoLoveShuang/auto-submit/raw/master/screenshots/ed6044e6.png)](https://github.com/ZimoLoveShuang/auto-submit/blob/master/screenshots/ed6044e6.png)
+
+4. 点左边的函数服务，新建云函数，名称随意，运行环境选择`python3.6`，创建方式选择空白函数，然后点击下一步 [![新建腾讯云函数](https://github.com/ZimoLoveShuang/auto-submit/raw/master/screenshots/a971478e.png)](https://github.com/ZimoLoveShuang/auto-submit/blob/master/screenshots/a971478e.png)
+
+5. 提交方法选择上传本地压缩包，把本地的/data，/utils，AccountInfo.ini，index.py，requirements.txt五个文件和文件夹打包上传，在点击下面的高级设置，设置内存为256M，超时时间为`30秒`，添加层为刚刚新建的函数依赖层，环境变量设置一个`TZ=Asia/Shanghai`，然后点击完成
+
+6. 进入新建好的云函数，左边点击触发管理，点击创建触发器，名称随意，触发周期选择自定义，然后配置cron表达式。下面的表达式表示每天中午十二点整执行，可配置多个时间以便早中晚自动运行
+
+   ```
+   0 0 12 * * * *
+   ```
+
+7. 然后就可以测试云函数了，绿色代表云函数执行成功，红色代表云函数执行失败（失败的原因大部分是由于依赖造成的）。返回结果是`auto submit fail.`代表自动提交失败；返回结果是`auto submit success.`，代表自动提交成功，如遇到问题，请仔细查看日志
+
+8. enjoy it!
+
+9. 也可配合Windows计划任务或者使用linux定时任务，将脚本挂在自己的云服务器上，不会就搜索一下，过程不再赘述
+
+#### 
+
